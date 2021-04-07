64f8632f186014b2541e140eb7d3315eaf715bdf
Colm Kenna <colm@stratait.ie>
2021-04-07 22:54:12 +0100

    Add Blszor Client

---
+++ src/BlazorClient/BlazorClient/Services/WeatherForcastService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BlazorClient.Data;
+
+namespace BlazorClient.Services
+{
+  public class WeatherForcastService: IWeatherForcastService
+  {
+    private readonly HttpClient _httpClient;
+
+    public WeatherForcastService(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<WeatherForecast>> GetWeatherForcast()
+    {
+      return await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>
+        (await _httpClient.GetStreamAsync($"weatherforecast"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+    }
+  }
+}
